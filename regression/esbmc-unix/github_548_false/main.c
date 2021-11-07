7865aa5b36f439927f5810976b24873918a3a241
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2021-11-07 13:22:47 +0000

    updated cswtich threashhold

--- regression/esbmc-unix/github_548_false/main.c
+++ regression/esbmc-unix/github_548_false/main.c
@@ -1,8 +1,4 @@
 #include <assert.h>
-extern void abort(void);
-void reach_error() { assert(0); }
-void __VERIFIER_assert(int cond) { if(!(cond)) { ERROR: {reach_error();abort();} } }
-
 #include<pthread.h>
 
 int glob1 = 0;
@@ -11,7 +7,6 @@ pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
 void *t_fun(void *arg) {
   pthread_mutex_lock(&mutex2);
   glob1 = 5;
-
   pthread_mutex_unlock(&mutex2);
   return NULL;
 }
@@ -19,14 +14,14 @@ void *t_fun(void *arg) {
 void *foo(void *arg)
 {
   pthread_mutex_lock(&mutex2); // wait/block mutex2
-  __VERIFIER_assert(glob1 == 0); // 
+  __ESBMC_assert(glob1 == 0, "glob1 can be 5"); // 
   pthread_mutex_unlock(&mutex2); // unlock mutex2
 }
 
 int main(void) {
-  pthread_t id, id2;  
+  pthread_t id;
   pthread_create(&id, NULL, t_fun, NULL);
-  *(foo)(0);
   pthread_join (id, NULL);  
+  *(foo)(0);
   return 0;
 }
